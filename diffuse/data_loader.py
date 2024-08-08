3d6cbea57327affdf07def13db4f0ebb39075fcc
jacopoiollo <jacopo.iollo@grenoble-inp.org>
2024-08-08 12:41:50 +0200

    unet

---
+++ diffuse/data_loader.py
@@ -0,0 +1,32 @@
+import jax
+import jax.numpy as jnp
+import pdb
+import matplotlib.pyplot as plt
+import einops
+from diffuse.unet import UNet
+from diffuse.score_matching import score_match_loss
+from diffuse.sde import SDE, LinearSchedule
+from functools import partial
+
+data = jnp.load("dataset/mnist.npz")
+key = jax.random.PRNGKey(0)
+xs = data["X"]
+batch_size = 64
+
+xs = jax.random.permutation(key, xs, axis=0)
+data = einops.rearrange(xs, "b h w -> b h w 1")
+# plt.imshow(data[0], cmap='gray')
+# plt.show()
+dt = jnp.linspace(0, 2.0, 200)
+dt = jnp.array([2.0 / 200] * batch_size)
+
+beta = LinearSchedule(b_min=0.02, b_max=5.0, t0=0.0, T=2.0)
+sde = SDE(beta)
+
+nn_unet = UNet(dt, 64)
+init_params = nn_unet.init(key, data[:batch_size], dt)
+
+res = nn_unet.apply(init_params, data[:batch_size], dt)
+
+loss = partial(score_match_loss, lmbda=lambda x: jnp.ones(x.shape), network=nn_unet)
+loss(init_params, key, data[:batch_size], sde, 100, 2.0)
---
+++ diffuse/train.py
@@ -0,0 +1,3 @@
+import jax
+import jax.numpy as jnp
+from diffuse.unet import UNet
