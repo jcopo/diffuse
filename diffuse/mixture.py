noiser variance bug fixed

diffuse/mixture.py
@@ -51,11 +51,11 @@ def pdf_mixtr(state: MixState, x: PyTreeDef):
 
 
 def init_mixture(key):
-    n_mixt = 5
+    n_mixt = 3
     d = 1
     means = jax.random.uniform(key, (n_mixt, d), minval=-3, maxval=3)
-    covs = 0.1 * (jax.random.normal(key, (n_mixt, d, d))) ** 2
-    mix_weights = jax.random.uniform(key, (n_mixt,))
+    covs = 0.1*(jax.random.normal(key+1, (n_mixt, d, d))) ** 2
+    mix_weights = jax.random.uniform(key+2, (n_mixt,))
     mix_weights /= jnp.sum(mix_weights)
 
     return MixState(means, covs, mix_weights)
@@ -69,7 +69,7 @@ def sampler_mixtr(key, state: MixState, N):
     key1, key2 = jax.random.split(key)
     idx = jax.random.choice(key1, jnp.arange(len(weights)), shape=(N,), p=weights)
     noise = jax.random.normal(key2, shape=(N, 1))
-    return mu[idx] + jnp.einsum("nij, ni->nj", sigma[idx], noise)
+    return mu[idx] + jnp.einsum("nij, ni->nj", jnp.sqrt(sigma[idx]), noise)
 
 
 def mixtr_sample(state: MixState, key):

diffuse/score_matching.py
@@ -85,9 +85,8 @@ def score_match_loss(
     # state = SDEState(all_paths, einops.repeat(ts, "n -> new_axis n", new_axis=n_x0))
     # (n_x0, n_ts, ...), (n_x0, n_ts, ...) -> (n_x0, n_ts, ...)
     # score_eval = jax.vmap(sde.score, in_axes=(1, None), out_axes=1)(state, state_0)
-
     sq_diff = einops.reduce(
-        (nn_eval - score_eval) ** 2, "t n ... -> t ", "mean"
+        (nn_eval - score_eval) ** 2, "t ... -> t ", "mean"
     )  # (n_ts)
 
     # mean_sq_diff = jnp.mean(sq_diff, axis=0)  # (n_ts, ...)
@@ -107,7 +106,7 @@ if __name__ == "__main__":
     sde = SDE(beta)
     key = jax.random.PRNGKey(0)
 
-    n_samples = 500
+    n_samples = 200
     mixt_state = init_mixture(key)
     samples_mixt = sampler_mixtr(key, mixt_state, n_samples)
 
@@ -116,7 +115,7 @@ if __name__ == "__main__":
         key,
         samples_mixt,
         sde,
-        100,
+        200,
         2.0,
         lambda x: jnp.ones(x.shape).squeeze(),
         model,

diffuse/sde.py
@@ -108,7 +108,7 @@ class SDE:
         alpha, beta = jnp.exp(-0.5 * int_b), 1 - jnp.exp(-int_b)
 
         rndm = jax.random.normal(key, x.shape)
-        res = alpha * x + beta * rndm
+        res = alpha * x + jnp.sqrt(beta) * rndm
         return SDEState(res, ts)
 
     def drift(self, state: SDEState) -> PyTreeDef:
@@ -129,7 +129,6 @@ class SDE:
             x, t = state
             beta_t = self.beta(tf - t)
             s = score(x, tf - t)
-            # jax.debug.print("{}", beta_t)
             return 0.5 * beta_t * x + beta_t * s
 
         def reverse_diffusion(state):

test/test_sde.py
@@ -7,6 +7,7 @@ from diffuse.mixture import (
     sampler_mixtr,
     display_histogram,
     init_mixture,
+    display_trajectories
 )
 from functools import partial
 import einops
@@ -67,7 +68,7 @@ def test_mixture():
     t_final = 2.0
 
     # move to the mixture
-    n_steps = 1000
+    n_steps = 100
     dts = jnp.array([t_final / n_steps] * (n_steps))
     beta = LinearSchedule(b_min=0.02, b_max=5.0, t0=0.0, T=2.0)
     sde = SDE(beta=beta)
@@ -95,26 +96,14 @@ def test_mixture():
     # plot samples
     # fig, axs = plt.subplots(1, 3, figsize=(15, 5))
     space = jnp.linspace(-3, 3, 200)
-    # pdf = jax.vmap(lambda x: pdf_mixtr(state, x))(space)
-    # axs[0].plot(space, pdf)
-    # #display_histogram(state_f.position, axs[0])
-    # display_histogram(samples_mixt, axs[0])
-    # display_histogram(state_0.position, axs[1])
-    # display_histogram(sample_mixt_T[0].position, axs[2])
-    # #axs[2].scatter(sample_0_T[0].position, jnp.zeros_like(state_0[0].position), color='red')
 
-    # print(state_0)
-
-    # for i in range(n_steps):
-    #     fig, axs = plt.subplots()
-    #     display_histogram(state_0[1].position[-i], axs)
-    #     #wait
-    #     plt.show(block=False)
-    #     plt.pause(0.1)
-    #     plt.close()
+    display_trajectories(sample_mixt_T.position.squeeze(), 100)
+    plt.show()
+    display_trajectories(state_Ts.position.squeeze(), 100)
+    plt.show()
 
     # PLOT FORWARD TRAJECTORIES
-    perct = [0, 0.1, 0.3, 0.7, 0.8, 0.9, 0.93, 0.9, 0.99, 1]
+    perct = [0, .03, .06, .08,  0.1, 0.3, 0.7, 0.8, 0.9, 1]
     n_plots = len(perct)
     fig, axs = plt.subplots(n_plots, 1, figsize=(10 * n_plots, n_plots))
     # end_particles = jnp.vstack([state_mixt.position, sample_mixt_T.position]).T