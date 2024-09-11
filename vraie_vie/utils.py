5c6d3d218ca19248b14ef8b35abeece920425c11
geoffroyO <geoffroy.oudoumanessah@gmail.com>
2024-09-11 22:00:09 +0200

    masque et fourier 👹

---
+++ vraie_vie/utils.py
@@ -0,0 +1,30 @@
+import jax
+import jax.numpy as jnp
+
+
+def slice_fourier(mri_slice):
+    return jnp.fft.fftshift(jnp.fft.fft2(mri_slice))
+
+
+def slice_inverse_fourier(fourier_transform):
+    return jnp.fft.ifft2(jnp.fft.ifftshift(fourier_transform))
+
+
+@jax.custom_vjp
+def generate_mask(key, w, s):
+    normalized_vector = w / w.sum()
+    subkey, _ = jax.random.split(key)
+    uniform_vector = jax.random.uniform(subkey, shape=w.shape, minval=0, maxval=1)
+    return jnp.where(s * normalized_vector < uniform_vector, 0, 1)
+
+
+def generate_mask_fwd(key, w, s):
+    mask = generate_mask(key, w, s)
+    return mask, None
+
+
+def generate_mask_bwd(_, grad_output):
+    return (None, grad_output, None)
+
+
+generate_mask.defvjp(generate_mask_fwd, generate_mask_bwd)
