03e4f0f681a35acecab97ed4496ecb5e1e162cfb
geoffroyO <geoffroy.oudoumanessah@gmail.com>
2024-09-16 17:29:57 +0200

    correct utils plz make it works 🎙️

--- diffuse/conditional.py
+++ diffuse/conditional.py
@@ -120,18 +120,18 @@ class CondSDE(SDE):
 def cond_reverse_drift(state: CondState, cond_sde: CondSDE) -> Array:
     # stack together x and y and apply reverse drift
     x, y, xi, t = state
-    # img = restore(xi, x, cond_sde.mask, y)
-    # return cond_sde.reverse_drift(SDEState(img, t))
-    drift_x = cond_sde.reverse_drift(SDEState(x, t))
-    beta_t = cond_sde.beta(cond_sde.tf - t)
-    meas_x = cond_sde.mask.measure(xi, x)
-    alpha_t = jnp.exp(cond_sde.beta.integrate(0.0, t))
+    img = cond_sde.mask.restore(xi, x, cond_sde.mask, y)
+    return cond_sde.reverse_drift(SDEState(img, t))
+    # drift_x = cond_sde.reverse_drift(SDEState(x, t))
+    # beta_t = cond_sde.beta(cond_sde.tf - t)
+    # meas_x = cond_sde.mask.measure(xi, x)
+    # alpha_t = jnp.exp(cond_sde.beta.integrate(0.0, t))
     # here if needed we average over y
-    drift_y = beta_t * (y - meas_x) / alpha_t
+    # drift_y = beta_t * (y - meas_x) / alpha_t
     # f = lambda y: beta_t * (y - meas_x) / alpha_t
     # drifts = jax.vmap(f)(y)
     # drift_y = drifts.mean(axis=0)
-    return drift_x + drift_y
+    # return drift_x + drift_y
 
 
 def cond_reverse_diffusion(state: CondState, cond_sde: CondSDE) -> Array:
