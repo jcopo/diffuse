1ccb8b36d7ba1ab7df3f4b2ed22adabfd9b0ee9d
jacopoiollo <jacopo.iollo@grenoble-inp.org>
2024-09-22 18:00:48 -0400

    factorizing  💀 👰🏻‍♂️

--- diffuse/optimizer.py
+++ diffuse/optimizer.py
@@ -13,29 +13,7 @@ import einops
 from diffuse.conditional import CondSDE
 from diffuse.sde import SDEState, euler_maryama_step
 from diffuse.filter import stratified
-
-
-def ess(log_weights: Array) -> float:
-    return jnp.exp(log_ess(log_weights))
-
-
-def log_ess(log_weights: Array) -> float:
-    """Compute the effective sample size.
-
-    Parameters
-    ----------
-    log_weights: 1D Array
-        log-weights of the sample
-
-    Returns
-    -------
-    log_ess: float
-        The logarithm of the effective sample size
-
-    """
-    return 2 * jsp.special.logsumexp(log_weights) - jsp.special.logsumexp(
-        2 * log_weights
-    )
+from diffuse.inference import calculate_past_contribution_score, calculate_drift_expt_post, logpdf_change_expected, logpdf_change_y, particle_step, logprob_y
 
 
 class ImplicitState(NamedTuple):
@@ -43,14 +21,6 @@ class ImplicitState(NamedTuple):
     cntrst_thetas: Array
     design: Array
     opt_state: optax.OptState
-
-
-def logprob_y(theta, y, design, cond_sde):
-    """
-    log p(y | theta, design)
-    """
-    f_y = cond_sde.mask.measure(design, theta)
-    return jax.scipy.stats.norm.logpdf(y, f_y, 1.)
 
 
 def information_gain(
@@ -81,142 +51,21 @@ def information_gain(
     #return (logprob_ref - logprob_means).mean(), y_ref
 
 
+def update_joint(sde_state, ys, ys_next, key, cond_sde, mask_history, dt):
+    drift_past = calculate_past_contribution_score(cond_sde, sde_state, mask_history, ys)
+    logpdf = partial(logpdf_change_y, y=ys, y_next=ys_next, drift_y=drift_past, cond_sde=cond_sde, dt=dt)
+    positions = particle_step(sde_state, key, drift_past, cond_sde, dt, ys, ys_next, logpdf)
 
-def calculate_drift_y(cond_sde:CondSDE, sde_state:SDEState, design:Array, y:Array):
-    r"""
-    Term
-    \beta(t)  \nabla_\thetab \log p(\yb^t|\thetab'_t, \xib)
-    to add for conditional diffusioon
-    """
-    x, t = sde_state
-    beta_t = cond_sde.beta(cond_sde.tf - t)
-    meas_x = cond_sde.mask.measure(design, x)
-    alpha_t = jnp.exp(cond_sde.beta.integrate(0., t))
-    drift_y = beta_t * cond_sde.mask.restore(design, jnp.zeros_like(x), (y - meas_x)) / alpha_t
-    return drift_y
+    return positions
 
 
-def calculate_past_contribution_score(cond_sde:CondSDE, sde_state:SDEState, mask_history:Array, y:Array):
-    r"""
-    Term
-    \beta(t)  \nabla_\thetab \log p(\yb^t|\thetab'_t, \xib)
-    to add for conditional diffusioon
-    """
-    x, t = sde_state
-    beta_t = cond_sde.beta(cond_sde.tf - t)
-    meas_x = cond_sde.mask.measure_from_mask(mask_history, x)
-    alpha_t = jnp.exp(cond_sde.beta.integrate(0., t))
-    drift_y = beta_t * cond_sde.mask.restore_from_mask(mask_history, jnp.zeros_like(x), (y - meas_x)) / alpha_t
-    return drift_y
+def update_expected_posterior(cntrst_sde_state, ys, ys_next, y_measured, key, cond_sde, mask_history, design, dt):
+    drift_past = calculate_past_contribution_score(cond_sde, cntrst_sde_state, mask_history, y_measured)
+    drift_y = calculate_drift_expt_post(cond_sde, cntrst_sde_state, design, ys)
+    logpdf = partial(logpdf_change_expected, y=ys, y_next=ys_next, drift_y=drift_past, cond_sde=cond_sde, dt=dt)
+    positions = particle_step(cntrst_sde_state, key, drift_y + drift_past, cond_sde, dt, ys, ys_next, logpdf)
 
-
-def calculate_drift_expt_post(cond_sde:CondSDE, sde_state:SDEState, design:Array, y:Array):
-    r"""
-    Term
-    \beta(t)  \sum_{n=1}^N \nu_n \nabla_\thetab \log p(\yb_n^t|\thetab'_t, \xib)
-    to add for conditional diffusioon
-    """
-    #pdb.set_trace()
-    drifts = jax.vmap(calculate_drift_y, in_axes=(None, None, None, 0))(cond_sde, sde_state, design, y)
-    #drifts = calculate_drift_y(cond_sde, t, xi, x, y)
-    drift_y = drifts.mean(axis=0)
-    return drift_y
-
-
-def particle_step(sde_state, rng_key, drift_y, cond_sde, dt, y, ys_next, logpdf):
-    def reverse_drift(state):
-        return cond_sde.reverse_drift(state) + drift_y
-
-    sde_state = euler_maryama_step(
-        sde_state, dt, rng_key, reverse_drift, cond_sde.reverse_diffusion
-    )
-    weights = jax.vmap(logpdf, in_axes=(SDEState(0, None),))(sde_state)
-    _norm = jax.scipy.special.logsumexp(weights, axis=0)
-    log_weights = weights - _norm
-    weights = jnp.exp(log_weights)
-
-    ess_val = ess(log_weights)
-    n_particles = sde_state.position.shape[0]
-    idx = stratified(rng_key, weights, n_particles)
-    return jax.lax.cond(ess_val < 0.5 * n_particles, lambda x: (x[idx], weights[idx]), lambda x: (x, weights[idx]), sde_state.position)
-    #return sde_state.position, weights
-
-
-def logpdf_change_y(x_sde_state:SDEState, y, y_next, drift_y:Array, cond_sde:CondSDE, dt):
-    r"""
-    log p(y_new | y_old, x_old)
-    with y_{k-1} | y_{k}, x_k ~ N(.| y_k + rev_drift*dt, sqrt(dt)*rev_diff)
-    """
-    cov = cond_sde.reverse_diffusion(x_sde_state) * jnp.sqrt(dt)
-    mean = y + (cond_sde.reverse_drift(x_sde_state) + drift_y) * dt
-    return jax.scipy.stats.multivariate_normal.logpdf(y_next, mean, cov).sum()
-
-
-def logpdf_change_expected(x_sde_state:SDEState, y, y_next, drift_y:Array, cond_sde:CondSDE, dt):
-    r"""
-    \sum log p(y_n | y_old, x_old) / N
-    with y_{k-1} | y_{k}, x_k ~ N(.| y_k + rev_drift*dt, sqrt(dt)*rev_diff)
-    """
-    logpdf = partial(logpdf_change_y, drift_y=drift_y, cond_sde=cond_sde, dt=dt)
-    logliks = jax.vmap(logpdf, in_axes=(None, 0, 0))(x_sde_state, y, y_next)
-    return logliks.mean()
-
-
-def generate_cond_sampleV2(
-    y: Array,
-    mask_history:Array,
-    key: PRNGKeyArray,
-    cond_sde: CondSDE,
-    x_shape: Tuple,
-    n_ts:int,
-    n_particles:int
-):
-    ts = jnp.linspace(0.0, cond_sde.tf, n_ts)
-    key_y, key_x = jax.random.split(key)
-
-    def update_joint(sde_state, ys, ys_next, key):
-        _, t = sde_state
-        dt = ys_next.t - ys.t
-        drift_past = calculate_past_contribution_score(cond_sde, sde_state, mask_history, ys.position)
-        logpdf = partial(logpdf_change_y, y=ys.position, y_next=ys_next.position, drift_y=drift_past, cond_sde=cond_sde, dt=dt)
-        positions, weights = particle_step(sde_state, key, drift_past, cond_sde, dt, ys, ys_next, logpdf)
-        return SDEState(positions, t + dt), weights
-
-    # generate path for y
-    y = einops.repeat(y, "... -> ts ... ", ts=n_ts)
-    state = SDEState(y, jnp.zeros((n_ts, 1)))
-    keys = jax.random.split(key_y, n_ts)
-    ys = jax.vmap(cond_sde.path, in_axes=(0, 0, 0))(keys, state, ts)
-
-    # u time reversal of y
-    us = SDEState(ys.position[::-1], ys.t)
-
-    u_0Tm = jax.tree.map(lambda x: x[:-1], us)
-    u_1T = jax.tree.map(lambda x: x[1:], us)
-
-    x_T = jax.random.normal(key_x, (n_particles, *x_shape))
-    state_x = SDEState(x_T, 0.)
-    weights = jnp.zeros((n_particles,))
-    # scan pcmc over x0 for n_steps
-    keys = jax.random.split(key, n_ts - 1)
-
-    def step(state, itr):
-        state_xt, weights = state
-        key, u, u_next = itr
-        keys = jax.random.split(key, n_ts)
-        n_state = update_joint(state_xt, u, u_next, key)
-        #n_state = jax.vmap(update_joint, in_axes=(SDEState(None, 0), 0, 0, 0))(state_xt, u, u_next, keys)
-        return n_state, n_state
-
-    end_state, hist = jax.lax.scan(step, (state_x, weights), (keys, u_0Tm, u_1T))
-
-    (positions, _), weights = hist
-    positions = jnp.concatenate([x_T[None], positions])
-    weights = jnp.concatenate([jnp.zeros((1, n_particles)), weights])
-
-
-    return end_state, (positions, weights)
-
+    return positions
 
 def impl_step(state:ImplicitState, rng_key: PRNGKeyArray, past_y:Array, mask_history:Array, cond_sde:CondSDE, optx_opt:GradientTransformation, ts:Array, dt:float):
 
@@ -229,15 +78,13 @@ def impl_step(state:ImplicitState, rng_key: PRNGKeyArray, past_y:Array, mask_his
     #   2 - get values y from samples of joint
     key_theta, key_cntrst = jax.random.split(rng_key)
 
-    def update_joint(sde_state, ys, ys_next, key):
-        drift_past = calculate_past_contribution_score(cond_sde, sde_state, mask_history, ys)
-        logpdf = partial(logpdf_change_y, y=ys, y_next=ys_next, drift_y=drift_past, cond_sde=cond_sde, dt=dt)
-        positions = particle_step(sde_state, key, drift_past, cond_sde, dt, ys, ys_next, logpdf)
+    def step_joint(sde_state, ys, ys_next, key):
+        positions = update_joint(sde_state, ys, ys_next, key, cond_sde, mask_history, dt)
         return positions
 
     keys_time = jax.random.split(key_theta, ts.shape[0]-1)
     sde_state = jax.tree_map(lambda x: x[1:], sde_state)
-    position, weights = jax.vmap(update_joint)(sde_state, past_y.position[:-1], past_y.position[1:], keys_time)
+    position, weights = jax.vmap(step_joint)(sde_state, past_y.position[:-1], past_y.position[1:], keys_time)
     thetas = jnp.concatenate([thetas[:1], position])
 
     # get ys
@@ -248,17 +95,14 @@ def impl_step(state:ImplicitState, rng_key: PRNGKeyArray, past_y:Array, mask_his
     # update expected posterior
     #   1 - use y values to update post
     #   2 - Get samples contrastive theta
-    def update_expected_posterior(cntrst_sde_state, ys, ys_next, y_measured, key):
-        drift_past = calculate_past_contribution_score(cond_sde, cntrst_sde_state, mask_history, y_measured)
-        drift_y = calculate_drift_expt_post(cond_sde, cntrst_sde_state, design, ys)
-        logpdf = partial(logpdf_change_expected, y=ys, y_next=ys_next, drift_y=drift_past, cond_sde=cond_sde, dt=dt)
-        positions = particle_step(cntrst_sde_state, key, drift_y + drift_past, cond_sde, dt, ys, ys_next, logpdf)
+    def step_expected_posterior(cntrst_sde_state, ys, ys_next, y_measured, key):
+        positions = update_expected_posterior(cntrst_sde_state, ys, ys_next, y_measured, key, cond_sde, mask_history, design, dt)
         return positions
 
     keys_time_c = jax.random.split(key_cntrst, ts.shape[0]-1)
     cntrst_sde_state = jax.tree_map(lambda x: x[1:], cntrst_sde_state)
     #pdb.set_trace()
-    position, weights = jax.vmap(update_expected_posterior)(cntrst_sde_state, ys[:-1], ys[1:], past_y.position[1:], keys_time_c)
+    position, weights = jax.vmap(step_expected_posterior)(cntrst_sde_state, ys[:-1], ys[1:], past_y.position[1:], keys_time_c)
     cntrst_thetas = jnp.concatenate([cntrst_thetas[:1], position])
 
 
@@ -268,6 +112,134 @@ def impl_step(state:ImplicitState, rng_key: PRNGKeyArray, past_y:Array, mask_his
     grad_xi_score = jax.grad(information_gain, argnums=2, has_aux=True)
     grad_xi, ys = grad_xi_score(
         thetas[-1], cntrst_thetas[-1], design, cond_sde
+    )
+    updates, opt_state = optx_opt.update(grad_xi, opt_state, design)
+    design = optax.apply_updates(design, updates)
+
+    return ImplicitState(thetas, cntrst_thetas, design, opt_state)
+
+
+def impl_one_step(state:ImplicitState, rng_key: PRNGKeyArray, past_y:Array, mask_history:Array, cond_sde:CondSDE, optx_opt:GradientTransformation, ts:Array, dt:float):
+
+    thetas, cntrst_thetas, design, opt_state = state
+    n_particles = thetas.shape[1]
+    n_cntrst_particles = cntrst_thetas.shape[1]
+    sde_state = SDEState(thetas, 0.)
+    cntrst_sde_state = SDEState(cntrst_thetas, 0.)
+
+    # update joint distribution
+    #   1 - conditional sde on joint -> samples theta
+    #   2 - get values y from samples of joint
+    key_theta, key_cntrst = jax.random.split(rng_key)
+
+    def step_joint(state, itr):
+        sde_state, weights = state
+        ys, ys_next, key = itr
+        _, t = sde_state
+        positions = update_joint(sde_state, ys, ys_next, key, cond_sde, mask_history, dt)
+        return (SDEState(positions, t + dt), weights), (positions, weights)
+
+    keys_time = jax.random.split(key_theta, ts.shape[0]-1)
+    position, weights = step_joint((sde_state, jnp.zeros((n_particles,))), (past_y.position[:-1], past_y.position[1:], keys_time))
+
+    # get ys
+    ys = jax.vmap(cond_sde.mask.measure, in_axes=(None, 0))(design, thetas)
+    #keys = jax.random.split(key_y, ts.shape[0])
+    #ys = jax.vmap(cond_sde.path, in_axes=(0, 0, 0))(keys, state, ts)
+
+    # update expected posterior
+    #   1 - use y values to update post
+    #   2 - Get samples contrastive theta
+    def step_expected_posterior(state, itr):
+        cntrst_sde_state, weights = state
+        _, t = cntrst_sde_state
+        ys, ys_next, y_measured, key = itr
+        positions = update_expected_posterior(cntrst_sde_state, ys, ys_next, y_measured, key, cond_sde, mask_history, design, dt)
+        return (SDEState(positions, t + dt), weights), (positions, weights)
+
+    keys_time_c = jax.random.split(key_cntrst, ts.shape[0]-1)
+    cntrst_sde_state = jax.tree_map(lambda x: x[1:], cntrst_sde_state)
+    #pdb.set_trace()
+
+    n_particles = cntrst_thetas.shape[1]
+    position, weights = step_expected_posterior((cntrst_sde_state, jnp.zeros((n_cntrst_particles,))), (ys[:-1], ys[1:], past_y.position[1:], keys_time_c))
+    cntrst_thetas = jnp.concatenate([cntrst_thetas[:1], position])
+
+
+    # get EIG gradient estimator
+    #  1 - evaluate score_f on thetas and contrastives_theta
+    #  2 - update design parameters with optax
+    grad_xi_score = jax.grad(information_gain, argnums=2, has_aux=True)
+    grad_xi, ys = grad_xi_score(
+        thetas[-1], cntrst_thetas[-1], design, cond_sde
+    )
+    updates, opt_state = optx_opt.update(grad_xi, opt_state, design)
+    design = optax.apply_updates(design, updates)
+
+    return ImplicitState(thetas, cntrst_thetas, design, opt_state)
+
+
+def impl_full_scan(state:ImplicitState, rng_key: PRNGKeyArray, past_y:Array, mask_history:Array, cond_sde:CondSDE, optx_opt:GradientTransformation, ts:Array, dt:float):
+
+    thetas, cntrst_thetas, design, opt_state = state
+    n_particles = thetas.shape[0]
+    n_cntrst_particles = cntrst_thetas.shape[0]
+    sde_state = SDEState(thetas, 0.)
+    cntrst_sde_state = SDEState(cntrst_thetas, 0.)
+
+    # update joint distribution
+    #   1 - conditional sde on joint -> samples theta
+    #   2 - get values y from samples of joint
+    key_theta, key_cntrst, key_y = jax.random.split(rng_key, 3)
+
+    def step_joint(state, itr):
+        sde_state, weights = state
+        _, t = sde_state
+        ys, ys_next, key = itr
+        positions, weights = update_joint(sde_state, ys, ys_next, key, cond_sde, mask_history, dt)
+
+        return (SDEState(positions, t + dt), weights), (positions, weights)
+
+    keys_time = jax.random.split(key_theta, ts.shape[0]-1)
+    # position, weights = jax.vmap(update_joint)(sde_state, past_y.position[:-1], past_y.position[1:], keys_time)
+
+    ((thetas, _), weights), hist = jax.lax.scan(step_joint, (sde_state, jnp.zeros((n_particles,))), (past_y.position[:-1], past_y.position[1:], keys_time))
+
+    # get ys
+    # ys output should be (n_t, n_particles, ...)
+    # and time noise -> measurement
+    thetas_hist, _ = hist
+    thetas_hist = jnp.concatenate([thetas[None], thetas_hist])
+
+    ys = jax.vmap(cond_sde.mask.measure, in_axes=(None, 0))(design, thetas_hist)
+    # ys = cond_sde.mask.measure(design, thetas)
+    # keys = jax.random.split(key_y, ts.shape[0])
+    # ys = jax.vmap(cond_sde.path, in_axes=(0, 0, 0))(keys, state, ts)
+
+
+    # update expected posterior
+    #   1 - use y values to update post
+    #   2 - Get samples contrastive theta
+    def step_expected_posterior(state, itr):
+        cntrst_sde_state, weights = state
+        _, t = cntrst_sde_state
+        ys, ys_next, y_measured, key = itr
+        positions, weights = update_expected_posterior(cntrst_sde_state, ys, ys_next, y_measured, key, cond_sde, mask_history, design, dt)
+
+        return (SDEState(positions, t + dt), weights), (positions, weights)
+
+
+    keys_time_c = jax.random.split(key_cntrst, ts.shape[0]-1)
+
+    ((cntrst_thetas, _), weights), hist = jax.lax.scan(step_expected_posterior, (cntrst_sde_state, jnp.zeros((n_cntrst_particles,))), (ys[:-1], ys[1:], past_y.position[1:], keys_time_c))
+
+
+    # get EIG gradient estimator
+    #  1 - evaluate score_f on thetas and contrastives_theta
+    #  2 - update design parameters with optax
+    grad_xi_score = jax.grad(information_gain, argnums=2, has_aux=True)
+    grad_xi, ys = grad_xi_score(
+        thetas[-1], cntrst_thetas, design, cond_sde
     )
     updates, opt_state = optx_opt.update(grad_xi, opt_state, design)
     design = optax.apply_updates(design, updates)
