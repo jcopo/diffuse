302dc61b3d66bb68566dc0999da2e2e2a4713e8f
geoffroyO <geoffroy.oudoumanessah@gmail.com>
2025-01-08 17:21:17 +0100

    reverse tenet ⏳

--- diffuse/integrator/deterministic.py
+++ diffuse/integrator/deterministic.py
@@ -44,7 +44,7 @@ class DPMpp2sIntegrator:
         self, position: Array, rng_key: PRNGKeyArray, t: float, dt: float
     ) -> EulerState:
         """Initialize integrator state with position, timestep and step size"""
-        return EulerState(position, jnp.array(self.sde.tf - t), jnp.array(-dt))
+        return EulerState(position, t, dt)
 
     def __call__(self, integrator_state: EulerState, score: Callable) -> EulerState:
         """Perform one DPM++-P2S integration step"""
@@ -54,9 +54,9 @@ class DPMpp2sIntegrator:
         mid_t = (t + current_t) / 2
 
         prev_int_b, current_int_b, mid_int_b = (
-            self.sde.beta.integrate(t, t0),
-            self.sde.beta.integrate(current_t, t0),
-            self.sde.beta.integrate(mid_t, t0),
+            self.sde.beta.integrate(self.sde.tf - t, t0),
+            self.sde.beta.integrate(self.sde.tf - current_t, t0),
+            self.sde.beta.integrate(self.sde.tf - mid_t, t0),
         )
         prev_alpha, current_alpha, mid_alpha = (
             jnp.exp(-0.5 * prev_int_b),
@@ -76,13 +76,13 @@ class DPMpp2sIntegrator:
         h = current_log_scale - prev_log_scale
         r = (mid_log_scale - prev_log_scale) / h
 
-        prev_tweedie = self.sde.tweedie(SDEState(position, t), score)
+        prev_tweedie = self.sde.tweedie(SDEState(position, self.sde.tf - t), score)
         u = (
             jnp.sqrt(mid_beta) / jnp.sqrt(prev_beta) * position
             + mid_alpha * (jnp.exp(-h * r) - 1) * prev_tweedie.position
         )
 
-        mid_tweedie = self.sde.tweedie(SDEState(u, mid_t), score)
+        mid_tweedie = self.sde.tweedie(SDEState(u, self.sde.tf - mid_t), score)
         D = (1 - 1 / (2 * r)) * prev_tweedie.position + (
             1 / (2 * r)
         ) * mid_tweedie.position
@@ -91,5 +91,7 @@ class DPMpp2sIntegrator:
             jnp.sqrt(current_beta) / jnp.sqrt(prev_beta) * position
             - current_alpha * (jnp.exp(-h) - 1) * D
         )
-        next_position = jnp.where(current_t < 1e-5, position, next_position)
+
+        # Handle edge case when t = tf
+        next_position = jnp.where((self.sde.tf - current_t) < 1e-5, position, next_position)
         return EulerState(next_position, current_t, dt)
