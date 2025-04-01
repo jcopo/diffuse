edffebc03b97f8e4061fb2e174b3e645a276ce5f
geoffroyO <geoffroy.oudoumanessah@gmail.com>
2025-04-01 18:43:29 +0200

    packaging

--- diffuse/timer/base.py
+++ diffuse/timer/base.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Timer:
     n_steps: int
-    def __call__(self, step: int) -> float:
-        ...
+
+    def __call__(self, step: int) -> float: ...
 
 
 @dataclass
@@ -15,6 +16,7 @@ class VpTimer(Timer):
     def __call__(self, step: int) -> float:
         return self.tf + step / self.n_steps * (self.eps - self.tf)
 
+
 @dataclass
 class HeunTimer(Timer):
     rho: float = 7.0
@@ -22,13 +24,22 @@ class HeunTimer(Timer):
     sigma_max: float = 80.0
 
     def __call__(self, step: int) -> float:
-        sigma_max_rho = self.sigma_max ** (1/self.rho)
-        sigma_min_rho = self.sigma_min ** (1/self.rho)
-        return (sigma_max_rho + step / self.n_steps * (sigma_min_rho - sigma_max_rho)) ** self.rho
+        sigma_max_rho = self.sigma_max ** (1 / self.rho)
+        sigma_min_rho = self.sigma_min ** (1 / self.rho)
+        return (
+            sigma_max_rho + step / self.n_steps * (sigma_min_rho - sigma_max_rho)
+        ) ** self.rho
 
 
 class DDIMTimer(Timer):
-    def __init__(self, n_steps: int, n_time_training: int, c_1: float = 0.001, c_2: float = 0.008, j0: int = 8):
+    def __init__(
+        self,
+        n_steps: int,
+        n_time_training: int,
+        c_1: float = 0.001,
+        c_2: float = 0.008,
+        j0: int = 8,
+    ):
         self.n_steps = n_steps
         self.n_time_training = n_time_training
         self.c_1 = c_1
@@ -41,10 +52,12 @@ class DDIMTimer(Timer):
         t = self.c_1 + (self.c_2 - self.c_1) * (step / self.n_steps) ** self.j0
         return t
 
+
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
     import jax.numpy as jnp
-    timer = HeunTimer(n_steps=100, sigma_max=1.)
+
+    timer = HeunTimer(n_steps=100, sigma_max=1.0)
     ts = jnp.linspace(0, 100, 100)
     plt.plot(ts, timer(ts))
     plt.show()
